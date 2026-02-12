3629fe0ec4f101df3c32684971b2847474f24156
prempalsingh1020 <prempalsingh030499@gmail.com>
2026-02-12 19:05:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault()
+	let r = document.querySelector("#radius");
+	//let btn = document.querySelector("#submit")
+	let volume = document.querySelector("#volume")
+    let V = (4/3)*(Math.PI*(r.value**3))
+		volume.value=V.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
